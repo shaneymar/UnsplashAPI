5d0a36b4e52c2552a73531360a575327f5fcf4cf
SHA <33160317+shaneymar@users.noreply.github.com>
2023-04-22 23:04:44 +0530

    search component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 
 
 function App() {
+
+  const [word, setWord] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+  console.log(word);
   return (
     <div>
       <Header title="Unsplash"/>
-      <Search />
+      <Search handleSubmit={handleSubmit} word={word} setWord={setWord} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({handleSubmit, word, setWord}) => {
+
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="seacrh for Image.." />
+                <Form.Control 
+                type="text"
+                value={word}
+                onChange={(e) => setWord(e.target.value)}
+                placeholder="seacrh for Image.."  
+                />
               </Col>
               <Col>
                 <Button type="submit" variant="primary">
